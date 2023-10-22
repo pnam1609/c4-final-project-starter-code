d082322db872c97f5a033e6a33a8066d7383456a
Hoang Phuong Nam <pnam16091999@gmail.com>
2023-10-22 22:26:31 +0700

    update file trvis.yml

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,6 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'a0a18mhsfg'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'xa000m3ogl'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/uat`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
